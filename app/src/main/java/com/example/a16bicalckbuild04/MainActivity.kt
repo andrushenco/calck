dba090925e43e0b39c3aead69bd1651e6e72c903
YAMAHA_YZF-R1 <ilay_ilay@inbox.ru>
2020-03-08 23:25:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a16bicalckbuild04/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a16bicalckbuild04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
